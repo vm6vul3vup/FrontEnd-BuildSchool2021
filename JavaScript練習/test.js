de6983cca02bdc3e11610a1c88e5ecaf320b6545
vm6vul3vup <vm6vul3vup@gmail.com>
2021-03-27 11:24:50 +0800

    JavaScript作業更新

---
+++ JavaScript練習/test.js
@@ -0,0 +1,17 @@
+let testname = "Tom";
+
+{
+    let testname = "Tommy";
+    console.log(testname);
+}
+
+console.log(testname);
+
+var testname = "Tom";
+
+{
+    var testname = "Tommy";
+    console.log(testname);
+}
+
+console.log(testname);
