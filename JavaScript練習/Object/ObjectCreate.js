d8844d84ddf971b9b12d8dad4254718604f80444
vm6vul3vup <vm6vul3vup@gmail.com>
2021-04-12 10:43:59 +0800

    上課練習

--- JavaScript練習/Object/ObjectCreate.js
+++ JavaScript練習/Object/ObjectCreate.js
@@ -7,6 +7,7 @@ console.log("The Object is " + person);
 console.log("This Object is %o", person);
 console.log(person.name);
 console.log(person["age"]);
+console.log("This Object is :" + JSON.stringify(person));
 
 let keys = Object.keys(person);
 let values = Object.values(person);
@@ -46,3 +47,23 @@ people.forEach((item,index) => {
     });
     console.log(msg);
 });
+
+var person2 = {
+    name: "Kevin",
+    age: 32,
+    height: 175,
+    weight: 64,
+    bmi: function () {
+    //object literal語法需要⽤用this才能存取到本⾝身屬性
+    return this.weight / ((this.height / 100) ** 2);
+    }
+    };
+    console.log(person2);
+    console.log(person2.bmi());
+
+
+var jsonText = JSON.stringify(person2);
+console.log(typeof jsonText);
+var p = JSON.parse(jsonText);
+console.log(p);
+console.log(typeof p);
---
+++ JavaScript練習/modules/helpers.js
@@ -0,0 +1,27 @@
+function $g (selector) {
+
+    //判斷是否為id selector
+    if (selector.includes('#') && !selector.includes('')){
+        //回傳Element
+        return document.querySelector(selector);
+    }
+
+    //回傳NodeList 集合
+    let nodelist = document.querySelectorAll(selector);
+
+    return nodelist.length == 1 ? nodelist[0] : nodelist;
+}
+
+function genUI(liArray){
+    let ul = document.createElement("ul");
+
+    liArray.forEach(value => {
+        let li = document.createElement("li");
+        li.innerText = value;
+        ul.appendChild(li);
+    });
+
+    return ul;
+}
+
+export { $g , genUI};
